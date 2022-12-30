f60fd12d71ec0b97e4af2a35ed4271a5fbb08fce
Merlijn van Uden <Merlijn-van-uden@hotmail.nl>
2022-12-30 17:25:47 +0100

    basic files + dark mode

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 'use strict'
 
-const switcher = document.querySelector('.btn')
+//
+const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)')
+if (prefersDarkScheme.matches) {
+    document.body.classList.add('dark-theme')
+} else {
+    document.body.classList.add('light-theme')
+}
 
+const switcher = document.querySelector('.btn')
 switcher.addEventListener('click', function () {
     document.body.classList.toggle('light-theme')
     document.body.classList.toggle('dark-theme')
